e8402b5b3e5284aa731822c384e5eb82c3a42f5c
Hao Yu <haoyu_cn@hotmail.com>
2017-11-16 10:45:12 +0000

    done

--- myProject/public/javascripts/comments.js
+++ myProject/public/javascripts/comments.js
@@ -1,8 +1,29 @@
 $(document).ready(function(){
   $("#postComment").click(function(){
-      var myobj = {Name:$("#name").val(),Gender:$("#gender").val()};
+      var myobj = {Name:$("#name").val(),Gender:$("#gender").val(),Vert:$("#vert").val(),Place:$("#place").val(),date:$("#date").val(),Pet:$("#pet").val()};
       jobj = JSON.stringify(myobj);
-      $("#json").text(jobj);
+/*      var personName = $("#name").val();
+      var nameforCheck;
+      nameforCheck = personName.toString();*/
+      var uniqueness;
+      uniqueness = 1;
+    $.getJSON('comment', function(data) {
+      console.log(data);
+      var uniqueness;
+      uniqueness = 1;
+      for(var comment in data) {
+	var everything;
+        com = data[comment];
+        everything = "{\"Name\":\"" + com.Name + "\",\"Gender\":\"" + com.Gender + "\",\"Vert\":\"" + com.Vert + "\",\"Place\":\"" + com.Place + "\",\"date\":\"" + com.date + "\",\"Pet\":\"" + com.Pet + "\"}";
+	if(jobj == everything){
+	  uniqueness = 0;
+	}
+	console.log(everything);
+	console.log(jobj);
+      }
+    })
+
+    if(uniqueness == 1){
       var url = "comment";
       $.ajax({
         url:url,
@@ -10,21 +31,96 @@ $(document).ready(function(){
         data: jobj,
         contentType: "application/json; charset=utf-8",
         success: function(data,textStatus) {
-          $("#done").html(textStatus);
         }
       })
+    }
+    else alert("This person already exist");
   });
 
   $("#getComments").click(function() {
+   var myobj = {Name:$("#name").val(),Gender:$("#gender").val(),Vert:$("#vert").val(),Place:$("#place").val(),date:$("#date").val(),Pet:$("#pet").val()};
+    jobj = JSON.stringify(myobj);
+
+      var url = "comment";
+      $.ajax({
+        url:url,
+        type: "POST",
+        data: jobj,
+        contentType: "application/json; charset=utf-8",
+        success: function(data,textStatus) {
+        }
+      })
+
+
+
     $.getJSON('comment', function(data) {
       console.log(data);
-      var everything = "<ul>";
+      var hightestScore = 0;
       for(var comment in data) {
-        com = data[comment];
-        everything += "<li> Name: " + com.Name + " -- Gender: " + com.Gender + "</li>";
+	var com = data[comment];
+        var currentScore = 0;
+       if(com.Name != ""){
+        if(com.Name == $("#name").val()){
+	  if(com.Gender != $("#gender").val()){
+            if(com.Vert == $("#vert").val()){
+              currentScore++;
+            }
+            if(com.Place == $("#place").val()){
+              currentScore++;
+            }
+            if(com.date == $("#date").val()){
+              currentScore++;
+            }
+            if(com.Pet == $("#Pet").val()){
+              currentScore++;
+            }
+          }
+        }
+	else {
+	  if(com.Vert == $("#vert").val()){
+              currentScore++;
+            }
+            if(com.Place == $("#place").val()){
+              currentScore++;
+            }
+            if(com.date == $("#date").val()){
+              currentScore++;
+            }
+            if(com.Pet == $("#Pet").val()){
+              currentScore++;
+            }
+        }
+       if(currentScore > hightestScore){
+         var everything;
+         everything += "<p>Your best Match:"
+         everything += "<p>  Name: " + com.Name + "</p>"; 
+         everything += "<p>  Gender: " + com.Gender + "</p>";
+         everything += "<p>  For getting relaxed: " + com.Vert + "</p>";
+         everything += "<p>  For palce to live: " + com.Place + "</p>";
+         everything += "<p>  For dating preference: " + com.date + "</p>";
+         everything += "<p>  For pet preference: " + com.Pet + "</p>";
+         everything += "<p>How well you two match?";  
+         if(currentScore == 4){
+           everything += "  100%</p>";
+         }
+         if(currentScore == 3){
+           everything += "  75%</p>";
+         }
+         if(currentScore == 2){
+           everything += "  50%</p>";
+         }
+         if(currentScore == 1){
+           everything += "  25%</p>";
+         }
+         if(currentScore == 0){
+           everything += "  0%</p>";
+         }
+         $("#comments").html(everything); 
+         hightestScore = currentScore;
+         currentScore = 0;
+       }
+       currentScore = 0;}
       }
-      everything += "</ul>";
-      $("#comments").html(everything);
     })
   })
 
@@ -34,7 +130,7 @@ $(document).ready(function(){
       url: url,
       type: "DELETE",
       success: function(){
-        $("#gender").html("");
+   
       }
 
     })
